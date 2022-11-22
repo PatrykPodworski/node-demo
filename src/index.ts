b5fc30ef7cedb91bd4e134cffd548786f1ea6a65
Patryk Podworski <patryk@xtramile.no>
2022-11-22 22:12:21 +0100

    Serve a file and use nodemon with ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = process.env["PORT"] || 3001;
 
 app.get("/", (_req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "public", "views", "index.html"));
 });
 
 app.listen(PORT, () => {
