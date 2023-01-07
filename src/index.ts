8c346d496fb1b7be6080374034c5cd7ee1dec4bb
Patryk Podworski <patryk@xtramile.no>
2023-01-07 14:06:25 +0100

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -12,18 +12,13 @@ const PORT = process.env["PORT"] || 3001;
 
 app.use(loggerMiddleware);
 app.use(corsMiddleware());
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(appRoot, "public")));
 
-app.get("/", (_req, res) => {
-  res.sendFile(path.join(appRoot, "views", "index.html"));
-});
-
 app.use(authRouter);
-
 app.use(notFoundRouter);
+
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
